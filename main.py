d0f41708bf7e858180e48bb4408ae4c2bf582047
Siebren Bakker <sbakker@salesforce.com>
2018-03-23 14:55:19 -0400

    Moved to testing with static buildpack

--- main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-    return redirect('https://fragforce.org', code=302)
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 443))
-    app.run(host='0.0.0.0', port=port)
